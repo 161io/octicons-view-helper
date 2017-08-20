c620cae8abb87d8b3d9724d015187760564d636b
Benoit Asselin <benoit@161.io>
2017-08-20 10:44:48 +0200

    phpunit bootstrap

---
+++ test/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Copyright (c) 161 SARL, https://161.io
+ */
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+} else {
+    // We are in vendor
+    require_once __DIR__ . '/../../../autoload.php';
+}
